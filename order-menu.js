316124657edc77eb352d947d65b57c2cee214d67
Maksim <amakeit@list.ru>
2019-10-03 17:15:01 +0500

    Доработка анкеты

---
+++ order-menu.js
@@ -0,0 +1,29 @@
+function changeRediz() {
+  var nameOrder = document.getElementById('name-of-order');
+  nameOrder.setAttribute('value', 'ВСЕ ВИДЫ РЕДИЗАЙНА');
+}
+
+function changeMeket() {
+  var nameOrder = document.getElementById('name-of-order');
+  nameOrder.setAttribute('value', 'РАЗРАБОТКА САЙТА ПО ТВОЕМУ МАКЕТУ');
+}
+
+function changeWtMaket() {
+  var nameOrder = document.getElementById('name-of-order');
+  nameOrder.setAttribute('value', 'РАЗРАБОТКА САЙТА БЕЗ МАКЕТА');
+}
+
+function changeMkMaket() {
+  var nameOrder = document.getElementById('name-of-order');
+  nameOrder.setAttribute('value', 'СОЗДАНИЕ МАКЕТА');
+}
+
+function changeDesignWork() {
+  var nameOrder = document.getElementById('name-of-order');
+  nameOrder.setAttribute('value', 'ГЛОБАЛЬНАЯ РАБОТА С ДИЗАЙНОМ');
+}
+
+function changeElse() {
+  var nameOrder = document.getElementById('name-of-order');
+  nameOrder.setAttribute('value', 'ДРУГОЕ');
+}
